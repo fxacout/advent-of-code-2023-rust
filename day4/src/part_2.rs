057dbb2892829bc0b02733315d7166479c15d40f
fxacout <fastcobos@gmail.com>
2023-12-06 13:09:31 +0100

    Day 4. Part 2

--- day4/src/part_2.rs
+++ day4/src/part_2.rs
@@ -1,2 +1,36 @@
+use std::{collections::HashMap};
+
 pub fn execute() {
+    let lines = include_str!("input.txt").lines();
+    let mut card_map: HashMap<usize, usize> = HashMap::new();
+    (1..lines.clone().collect::<Vec<&str>>().len() + 1).for_each(|line| {
+        card_map.insert(line, 1);
+    });
+
+    let result: usize = lines
+    .map(|line| line.split(": ").collect::<Vec<&str>>().get(1).unwrap().to_owned().to_owned()
+    )
+    .enumerate()
+    .map(|(index, line)| {
+        let splitted: Vec<&str> = line.split("|").collect::<Vec<&str>>();
+        let winning_numbers = splitted.get(0).unwrap().split(" "). map(
+            |number| number.parse::<i32>().unwrap_or(-1)
+        ).collect::<Vec<i32>>();
+        let hits = splitted.get(1).unwrap().split(" ").fold(0, |acc, number| {
+            if winning_numbers.contains(&number.parse::<i32>().unwrap_or(-2)) {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+        if hits > 0 {
+            (index+2..(index + hits+2)).for_each(|x| {
+                card_map.insert(x, card_map.get(&(x)).unwrap() + card_map.get(&(index + 1)).unwrap());
+            });
+        }
+        card_map.get(&(index + 1)).unwrap().clone()
+    }
+    ).sum();
+    println!("Card Map: {:?}", card_map);
+    println!("Result Part 2: {}", result);
 }
